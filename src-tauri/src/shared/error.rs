33c460bbda3a9c5deea519bb9f74c1909eab517e
Ariya Moheb <ariya.mms@gmail.com>
2025-11-06 19:26:33 +0330

    Define app error types
    This commit introduces a new module for shared error handling
    types. It provides a standardized way to manage and serialize errors
    within the application.
    
    - AppResult: A type alias for anyhow::Result for internal use.
    - AppSerializableResult: A Result type that uses AppSerializableError.
    - AppSerializableError: A serializable error type that wraps
      anyhow::Error messages, making them suitable for communication
      with the Tauri frontend.

---
+++ src-tauri/src/shared/error.rs
@@ -0,0 +1,27 @@
+use serde::Serialize;
+
+/// A type alias for `Result` that uses `AppError` as the error type.
+///
+/// This is used for internal functions where the error does not need to be serialized.
+pub type AppResult<T> = anyhow::Result<T>;
+
+pub type AppSerializableResult<T> = Result<T, AppSerializableError>;
+
+/// A serializable error type for use in Tauri commands.
+///
+/// `anyhow::Error` is not `Serialize`, so we convert it to this simple string-based
+/// error at the boundary of our application (in the `invoke_handler`).
+///
+/// TODO: polish doc
+#[derive(Debug, Serialize)]
+struct AppSerializableError {
+    pub message: String,
+}
+
+impl From<anyhow::Error> for AppSerializableError {
+    fn from(error: anyhow::Error) -> Self {
+        Self {
+            message: error.to_string(),
+        }
+    }
+}
