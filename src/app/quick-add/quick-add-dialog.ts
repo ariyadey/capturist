1006bea69ffdf92775c20f13f56f79dee5903b0b
Ariya Moheb <ariya.mms@gmail.com>
2025-11-08 03:50:14 +0330

    Display loading spinner on quick add
    - Disable the form and submit button while a task is being added.
    - Show a loading spinner on the submit button to indicate progress.
    - Re-enable the form and reset it once the submission is complete,
      regardless of success or failure.
    - This improves user experience by providing visual feedback during
      the asynchronous task submission process.

--- src/app/quick-add/quick-add-dialog.ts
+++ src/app/quick-add/quick-add-dialog.ts
@@ -1,11 +1,13 @@
 import { CdkTextareaAutosize } from "@angular/cdk/text-field";
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject, signal } from "@angular/core";
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButton } from "@angular/material/button";
 import { MatDialogActions, MatDialogClose, MatDialogContent } from "@angular/material/dialog";
 import { MatDivider } from "@angular/material/divider";
 import { MatFormField } from "@angular/material/form-field";
 import { MatInput } from "@angular/material/input";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { Todoist } from "@cpt/shared/external/todoist";
 import { TodoistRequestError } from "@doist/todoist-api-typescript";
 
@@ -24,6 +26,9 @@ import { TodoistRequestError } from "@doist/todoist-api-typescript";
     MatDialogClose,
     CdkTextareaAutosize,
     MatDivider,
+    MatProgressSpinnerModule,
+    CommonModule,
+
   ],
 })
 export class QuickAddDialog {
@@ -32,8 +37,11 @@ export class QuickAddDialog {
     name: ["", Validators.required],
     description: [""],
   });
+  protected readonly isAdding = signal(false);
 
   onSubmit() {
+    this.form.disable();
+    this.isAdding.set(true);
     this.todoistApi
       .quickAddTask({
         text: this.form.getRawValue().name,
@@ -45,6 +53,10 @@ export class QuickAddDialog {
         // TODO: 05/09/2025 Show error message
         // TODO: 08/11/2025 ctrl+enter handling
       })
-      .then(_ => this.form.reset());
+      .finally(() => {
+        this.isAdding.set(false);
+        this.form.reset();
+        this.form.enable();
+      });
   }
 }
